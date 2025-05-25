79106d76ca15440df2fc4cd01cee9943687276d6
Rasmoose-dev <rasmoose-dev@gmail.com>
2025-05-25 20:50:32 +0200

    Connect skill button to GameManager

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,8 +1,18 @@
 extends Control
 
 
+
 func _on_start_button_pressed() -> void:
 	GameManager.start_game()
 
+
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_skill_tree_button_pressed() -> void:
+	show_skill_tree()
+
+func show_skill_tree():
+	pass
--- viking/viking_state.gd
+++ viking/viking_state.gd
@@ -5,6 +5,7 @@ class_name VikingState
 @export var max_health: int = 10
 @export var move_speed: float = 48.0
 @export var axe_throw_speed: float = 500.0  
+@export var skills: Dictionary[String, int] = {}
 
 func start() -> void:
-    health = max_health
+	health = max_health
